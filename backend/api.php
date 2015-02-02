180d08cd3a22dc43db17f0ad3c00425f802dd04d
mpfeil <matthias.pfeil@wwu.de>
2015-02-02 16:25:07 +0100

    implements function to query speeches by room and date

--- backend/api.php
+++ backend/api.php
@@ -2,47 +2,44 @@
   // attempt a connection
   ini_set('display_errors', '1');
   error_reporting(E_ALL | E_STRICT);
-  include("config.php");
+  require_once('config.php');
 
-  //-----------------------------------------------------------
-
-  // execute query
-  $sql = "SELECT title, start , subtitle, description, duration
-      FROM Speech
-      WHERE room_id =  'Aula'
-      AND DATE =  '2015-03-11'
-      Order by start";
-
-  //-----------------------------------------------------------
-
-  $result = mysqli_query($connection, $sql);
-
-
-  // iterate over result set
-  // print each row
-  $number = 0;
-  $array = [];
-  while ($row = mysqli_fetch_array($result)) {
-    $title = (string)$row[0];
-    $start = (string)$row[1];
-    $subtitle = (string)$row[2];
-    $description = (string)$row[3];
-    $number = $number + 1;
-    $duration = (string)$row[4];
-
-    $test = new stdClass();
-    $test->title = (string)$row[0];
-    $test->start = (string)$row[1];
-    $test->subtitle = (string)$row[2];
-    $test->description = (string)$row[3];
-    $test->number = $number+1;
-    $test->duration = (string)$row[4];
-
-    array_push($array, $test);
+  if (isset($_GET["func"])) {
+    if ($_GET["func"] == "getSpeeches") {
+      $room = $_GET["room"];
+      $date = $_GET["date"];
+      echo getSpeeches($connection,$room,$date);
+    }
   }
 
-  print_r(json_encode($array));
+  function getSpeeches($connection,$room, $date) {
 
-  // close connection
-  mysqli_close($connection);
+    $sql = "SELECT title, start , subtitle, description, duration
+      FROM Speech
+      WHERE room_id = '".$room."'
+      AND DATE =  '".$date."'
+      Order by start";
+
+    $result = mysqli_query($connection, $sql);
+
+    $number = 0;
+    $array = [];
+    while ($row = mysqli_fetch_array($result)) {
+
+      $test = new stdClass();
+      $test->title = (string)$row[0];
+      $test->start = (string)$row[1];
+      $test->subtitle = (string)$row[2];
+      $test->description = (string)$row[3];
+      $test->number = $number++;
+      $test->duration = (string)$row[4];
+
+      array_push($array, $test);
+    }
+
+    // close connection
+    mysqli_close($connection);
+
+    return json_encode($array);
+  }
 ?>
