2fc0c0396066488a245b3ea8c654392d26cb0564
joanago <j_gock02@uni-muenster.de>
2015-02-23 17:03:06 +0100

    neue Suchfunktion

--- backend/api.php
+++ backend/api.php
@@ -13,6 +13,10 @@
 	if ($_GET["func"] == "getTitles") {
 		echo getTitles($connection);
 	}
+	if ($_GET["func"] == "getSearch") {
+	  $search = $_GET["search"];
+	  echo getSearch($connection,$search);
+	}
   }
   
   function getTitles($connection) {
@@ -74,6 +78,41 @@
       On SpeakerSpeech.speaker_id = Speaker.id
       WHERE room_id = '".$room."'
       AND DATE =  '".$date."'
+      GROUP BY start
+      Order by start";
+
+    $result = mysqli_query($connection, $sql);
+
+    $number = 0;
+    $array = [];
+    while ($row = mysqli_fetch_array($result)) {
+
+      $test = new stdClass();
+      $test->title = (string)$row[0];
+      $test->start = (string)$row[1];
+      $test->subtitle = (string)$row[2];
+      $test->description = (string)$row[3];
+      $test->number = $number++;
+      $test->duration = (string)$row[4];
+      $test->name = (string)$row[5];
+
+      array_push($array, $test);
+    }
+
+    // close connection
+    mysqli_close($connection);
+
+    return json_encode($array);
+  }
+  
+  function getSearch($connection,$search) {
+
+    $sql = "SELECT title, start , subtitle, description, duration, GROUP_CONCAT(name) FROM Speech
+      LEFT OUTER JOIN SpeakerSpeech
+      ON Speech.id = SpeakerSpeech.speech_id
+      LEFT OUTER JOIN Speaker
+      On SpeakerSpeech.speaker_id = Speaker.id
+      WHERE title LIKE '".$search."%'
       GROUP BY start
       Order by start";
 
