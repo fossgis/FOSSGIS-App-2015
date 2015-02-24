a93c8654269403fd7da22866e2e4b24f3f4d1d87
ttass01 <tobias.tasse@googlemail.com>
2015-02-24 19:02:53 +0100

    iCal Export Final #2
    minus Error report

--- backend/iCalExport.php
+++ backend/iCalExport.php
@@ -1,7 +1,7 @@
 <?php
 
-	ini_set('display_errors', '1');
-	error_reporting(E_ALL | E_STRICT);
+	//ini_set('display_errors', '1');
+	//error_reporting(E_ALL | E_STRICT);
 	
 	// Get the cookies out of your browser
 	$ititles = $_COOKIE['title'];
