256177a05eb049f3592eeb5ac2c3e76289dca82a
mpfeil <matthias.pfeil@wwu.de>
2015-02-27 12:54:07 +0100

    set correct id for feedback iframe

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -59,16 +59,17 @@
         tab = target+"-7"
         break;
     }
-    $(target+" > .tabs-content > "+tab).append("<div class='row' id='infos"+speech.number+"'><div class='small-12 medium-6 large-8 columns'><p>"+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-6 large-4 columns'><form action='../backend/teilnehmen.php' method='get'><input type=hidden id=titleid name=titleid value="+speech.id+"><a href='#' class='button openmodal' style='width: 64%; padding: 0.001rem 0rem' data-reveal-id='infos"+speech.number+"-"+target.slice(1,target.length)+"'>weitere Informationen</a> <input type='submit' id='filter' class='button' style='width: 34%; padding: 0.001rem 0rem' value='Vormerken'></form></div></div>");
-    $(target+" > .tabs-content > "+tab).append("<div id='infos"+speech.number+"-"+target.slice(1,target.length)+"' class='reveal-modal' data-reveal><h2>"+speech.title+"</h2><p class='lead'>"+speech.subtitle+"</p><p>Dauer: "+speech.duration+"</p><p>Referent: "+speech.speaker+"</p><p class='text-justify'>"+speech.description+"</p><a class='rateButton button expand' style='font-weight: bold;'>Vortrag bewerten</a><a class='close-reveal-modal'>&#215;</a></div>");
+    $(target+" > .tabs-content > "+tab).append("<div class='row' id='infos"+speech.number+"-"+speech.id+"'><div class='small-12 medium-6 large-8 columns'><p>"+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-6 large-4 columns'><form action='../backend/teilnehmen.php' method='get'><input type=hidden id=titleid name=titleid value="+speech.id+"><a href='#' class='button openmodal' style='width: 64%; padding: 0.001rem 0rem' data-reveal-id='infos"+speech.number+"-"+target.slice(1,target.length)+"-"+speech.id+"'>weitere Informationen</a> <input type='submit' id='filter' class='button' style='width: 34%; padding: 0.001rem 0rem' value='Vormerken'></form></div></div>");
+    $(target+" > .tabs-content > "+tab).append("<div id='infos"+speech.number+"-"+target.slice(1,target.length)+"-"+speech.id+"' class='reveal-modal' data-reveal><h2>"+speech.title+"</h2><p class='lead'>"+speech.subtitle+"</p><p>Dauer: "+speech.duration+"</p><p>Referent: "+speech.speaker+"</p><p class='text-justify'>"+speech.description+"</p><a class='rateButton button expand' style='font-weight: bold;'>Vortrag bewerten</a><a class='close-reveal-modal'>&#215;</a></div>");
   }
 
   $(document).on('click','.rateButton', function(evt){
-    var id = evt.target.parentElement.id.split("-")[0];
-    var fbid = $('#'+id+' input[type=hidden]').val();
-    console.log(fbid);
+    var split = evt.target.parentElement.id.split("-");
+    var id = evt.target.parentElement.id.split("-")[split.length-1];
+    /*var fbid = $('#'+id+' input[type=hidden]').val();
+    console.log(fbid);*/
     if ($('#rateFrame').length === 0) {
-      $(evt.target.parentElement).append("<iframe id='rateFrame' width='100%' height='250px' frameborder='0' src='http://pb.fossgis.de/feedback/FOSSGIS2014/event/"+fbid+".de.html' style='overflow: auto'></iframe>");
+      $(evt.target.parentElement).append("<iframe id='rateFrame' width='100%' height='250px' frameborder='0' src='http://pb.fossgis.de/feedback/FOSSGIS2014/event/"+id+".de.html' style='overflow: auto'></iframe>");
     }
   });
 
@@ -87,8 +88,8 @@ function showMyEvents () {
       var obj = JSON.parse(data);
       $("#myevent").html("");
       obj.forEach(function (speech) {
-        $("#myevent").append("<div class='row'><div class='small-12 medium-8 large-9 columns'><p>" +speech.datum+" "+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-4 large-3 columns'><a href='#' class='button openmodal' style='width: 100%; padding: 0.001rem 0rem' data-reveal-id='infos"+speech.number+"-myevents'>weitere Informationen</a></div></div>");
-        $("#myevent").append("<div id='infos"+speech.number+"-myevents' class='reveal-modal' data-reveal><h2>"+speech.title+"</h2><p class='lead'>Raum: "+speech.room+"</p><p>Dauer: "+speech.duration+"</p><p class='text-justify'>"+speech.description+"</p><a class='rateButton button expand' style='font-weight: bold;'>Vortrag bewerten</a><a class='close-reveal-modal'>&#215;</a></div>");
+        $("#myevent").append("<div class='row' id='infos"+speech.number+"-myevents'><div class='small-12 medium-8 large-9 columns'><p>" +speech.datum+" "+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-4 large-3 columns'><a href='#' class='button openmodal' style='width: 100%; padding: 0.001rem 0rem' data-reveal-id='infos"+speech.number+"-myevents-"+speech.id+"'>weitere Informationen</a></div></div>");
+        $("#myevent").append("<div id='infos"+speech.number+"-myevents-"+speech.id+"' class='reveal-modal' data-reveal><h2>"+speech.title+"</h2><p class='lead'>Raum: "+speech.room+"</p><p>Dauer: "+speech.duration+"</p><p class='text-justify'>"+speech.description+"</p><a class='rateButton button expand' style='font-weight: bold;'>Vortrag bewerten</a><a class='close-reveal-modal'>&#215;</a></div>");
       });
     }).fail(function (err) {
       console.log(err);
@@ -131,8 +132,8 @@ function showMyEvents () {
         var obj = JSON.parse(data);
         $("#mysearch").html("");
         obj.forEach(function (speech) {
-          $("#mysearch").append("<div class='row'><div class='small-12 medium-6 large-8 columns'><p>"+speech.date+" "+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-6 large-4 columns'><form action='../backend/teilnehmen.php' method='get'><input type=hidden id=titleid name=titleid value="+speech.id+"><a href='#' class='button openmodal' style='width: 64%; padding: 0.001rem 0rem' data-reveal-id='infos"+speech.number+"-search'> weitere Informationen</a> <input type='submit' id='filter' class='button' style='width: 34%; padding: 0.001rem 0rem' value='Vormerken'></form></div></div>");
-          $("#mysearch").append("<div id='infos"+speech.number+"-search' class='reveal-modal' data-reveal><h2>"+speech.title+"</h2><p class='lead'>"+speech.subtitle+"</p><p>Raum: "+speech.name+"</p><p>Dauer: "+speech.duration+"</p><p>Referent: "+speech.speaker+"</p><p class='text-justify'>"+speech.description+"</p><a class='rateButton button expand' style='font-weight: bold;'>Vortrag bewerten</a><a class='close-reveal-modal'>&#215;</a></div>");
+          $("#mysearch").append("<div class='row' id='infos-"+speech.number+"-search'><div class='small-12 medium-6 large-8 columns'><p>"+speech.date+" "+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-6 large-4 columns'><form action='../backend/teilnehmen.php' method='get'><input type=hidden id=titleid name=titleid value="+speech.id+"><a href='#' class='button openmodal' style='width: 64%; padding: 0.001rem 0rem' data-reveal-id='infos-"+speech.number+"-search-"+speech.id+"'> weitere Informationen</a> <input type='submit' id='filter' class='button' style='width: 34%; padding: 0.001rem 0rem' value='Vormerken'></form></div></div>");
+          $("#mysearch").append("<div id='infos-"+speech.number+"-search-"+speech.id+"' class='reveal-modal' data-reveal><h2>"+speech.title+"</h2><p class='lead'>"+speech.subtitle+"</p><p>Raum: "+speech.name+"</p><p>Dauer: "+speech.duration+"</p><p>Referent: "+speech.speaker+"</p><p class='text-justify'>"+speech.description+"</p><a class='rateButton button expand' style='font-weight: bold;'>Vortrag bewerten</a><a class='close-reveal-modal'>&#215;</a></div>");
       });
       if (obj.length == 0) {
         $("#mysearch").append("<div class='row'><div class='small-12 columns'>Zu dieser Suchanfrage gibt es leider keine Ergebnisse...</div></div><br/>")
