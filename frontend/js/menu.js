e4623d26117a466fe1f6097a850ba81388a17102
Ehrecke <ehrecke.jan@gmail.com>
2015-03-02 18:39:13 +0100

    small fix menu.js

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -90,7 +90,7 @@ function showMyEvents () {
       var obj = JSON.parse(data);
       $("#myevent").html("");
       obj.forEach(function (speech) {
-        $("#myevent").append("<div class='row' id='infos"+speech.number+"-myevents'><div class='small-12 medium-8 large-9 columns'><p>" +speech.datum+" "+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-4 large-3 columns'><a href='#fndtn-veranstaltungen' class='button openmodal' style='width: 100%; padding: 0.001rem 0rem' data-reveal-id='infos"+speech.number+"-myevents-"+speech.id+"'>weitere Informationen</a></div></div>");
+        $("#myevent").append("<div class='row' id='infos"+speech.number+"-myevents'><div class='small-12 medium-8 large-9 columns'><p>" +speech.datum+" - "+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-4 large-3 columns'><a href='#fndtn-veranstaltungen' class='button openmodal' style='width: 100%; padding: 0.001rem 0rem' data-reveal-id='infos"+speech.number+"-myevents-"+speech.id+"'>weitere Informationen</a></div></div>");
         $("#myevent").append("<div id='infos"+speech.number+"-myevents-"+speech.id+"' class='reveal-modal' data-reveal style='max-height: 95%; overflow: scroll'><h2>"+speech.title+"</h2><p class='lead'>Raum: "+speech.room+"</p><p>Dauer: "+speech.duration+"</p><p class='text-justify'>"+speech.description+"</p><a class='close-reveal-modal'>&#215;</a></div>");
       });
     }).fail(function (err) {
@@ -134,7 +134,7 @@ function showMyEvents () {
         var obj = JSON.parse(data);
         $("#mysearch").html("");
         obj.forEach(function (speech) {
-          $("#mysearch").append("<div class='row' id='infos-"+speech.number+"-search'><div class='small-12 medium-6 large-8 columns'><p>"+speech.date+" "+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-6 large-4 columns'><form action='../backend/teilnehmen.php' method='get'><input type=hidden id=titleid name=titleid value="+speech.id+"><a href='#fndtn-veranstaltungen' class='button openmodal' style='width: 64%; padding: 0.001rem 0rem' data-reveal-id='infos-"+speech.number+"-search-"+speech.id+"'>weitere Informationen</a> <input type='submit' id='filter' class='button' style='width: 34%; padding: 0.001rem 0rem' value='Vormerken'></form></div></div>");
+          $("#mysearch").append("<div class='row' id='infos-"+speech.number+"-search'><div class='small-12 medium-6 large-8 columns'><p>"+speech.date+" - "+speech.start+" : "+speech.title+"</p></div><div class='small-12 medium-6 large-4 columns'><form action='../backend/teilnehmen.php' method='get'><input type=hidden id=titleid name=titleid value="+speech.id+"><a href='#fndtn-veranstaltungen' class='button openmodal' style='width: 64%; padding: 0.001rem 0rem' data-reveal-id='infos-"+speech.number+"-search-"+speech.id+"'>weitere Informationen</a> <input type='submit' id='filter' class='button' style='width: 34%; padding: 0.001rem 0rem' value='Vormerken'></form></div></div>");
           $("#mysearch").append("<div id='infos-"+speech.number+"-search-"+speech.id+"' class='reveal-modal' data-reveal style='max-height: 95%; overflow: scroll'><h2>"+speech.title+"</h2><p class='lead'>"+speech.subtitle+"</p><p>Raum: "+speech.name+"</p><p>Dauer: "+speech.duration+"</p><p>Referent: "+speech.speaker+"</p><p class='text-justify'>"+speech.description+"</p><a class='close-reveal-modal'>&#215;</a></div>");
       });
       if (obj.length == 0) {
